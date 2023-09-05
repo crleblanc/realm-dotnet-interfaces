9a8c20c61e3a8ff907496fe066aa61da6ef134b6
Chris LeBlanc <crleblanc@gmail.com>
2023-09-05 13:39:06 -0700

    Added examples files, the framework 4.8 files do not compile

---
+++ realm-dotnet-7/Program.cs
@@ -0,0 +1,25 @@
+﻿using Realms;
+
+namespace RealmInterfaces
+{
+    public partial class SubclassRealmEntity : RealmObject
+    {
+        [PrimaryKey]
+        public MongoDB.Bson.ObjectId Id { get; set; }
+        public int Counter { get; set; }
+    }
+
+    public partial class ImplementsRealmInterface : IRealmObject
+    {
+        [PrimaryKey]
+        public MongoDB.Bson.ObjectId Id { get; set; }
+        public int Counter { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ realm-dotnet-framework-4.8/Program.cs
@@ -0,0 +1,26 @@
+﻿using Realms;
+
+namespace RealmInterfaces
+{
+    public partial class SubclassRealmEntity : RealmObject
+    {
+        [PrimaryKey]
+        public MongoDB.Bson.ObjectId Id { get; set; }
+        public int Counter { get; set; }
+    }
+
+    // We cannot use IRealmObject here because of CS0535 errors:
+    public partial class ImplementsRealmInterface : IRealmObject
+    {
+        [PrimaryKey]
+        public MongoDB.Bson.ObjectId Id { get; set; }
+        public int Counter { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
